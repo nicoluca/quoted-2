736aaca2d120fceaaa5fb6a1c8cab2a66929855c
Nico <nico.pfau@icloud.com>
2023-07-04 11:55:30 +0200

    Initial commit

---
+++ src/main/java/org/nico/quoted/QuotedApplication.java
@@ -0,0 +1,13 @@
+package org.nico.quoted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nico/quoted/QuotedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nico.quoted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
