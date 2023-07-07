ec98701ee575d9f6a81c038dec7e3331975cda7f
Nico <nico.pfau@icloud.com>
2023-07-07 13:34:59 +0200

    feat: search quote endpoint

--- src/main/java/org/nico/quoted/repository/QuoteRepository.java
+++ src/main/java/org/nico/quoted/repository/QuoteRepository.java
@@ -2,11 +2,15 @@ package org.nico.quoted.repository;
 
 import org.nico.quoted.domain.Quote;
 import org.nico.quoted.projections.QuoteProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "quotes", path = "quotes", excerptProjection = QuoteProjection.class)
 public interface QuoteRepository extends PagingAndSortingRepository<Quote, Long> {
+    Page<Quote> findByTextContainingIgnoreCase(@RequestParam("text") String text, Pageable pageable);
 }
