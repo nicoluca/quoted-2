f8e436874be69adecffe144aafe3432c297b4cf3
Nico <nico.pfau@icloud.com>
2023-07-07 10:17:43 +0200

    PageRepository

--- src/main/java/org/nico/quoted/repository/QuoteRepository.java
+++ src/main/java/org/nico/quoted/repository/QuoteRepository.java
@@ -2,10 +2,11 @@ package org.nico.quoted.repository;
 
 import org.nico.quoted.domain.Quote;
 import org.nico.quoted.projections.QuoteProjection;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-// TODO allow cross origin
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "quotes", path = "quotes", excerptProjection = QuoteProjection.class)
-public interface QuoteRepository extends CrudRepository<Quote, Long> {
+public interface QuoteRepository extends PagingAndSortingRepository<Quote, Long> {
 }
