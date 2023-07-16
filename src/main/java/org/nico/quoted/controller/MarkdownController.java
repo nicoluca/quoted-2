b329c983ef608224c4f000a6e2d54d566b1b4860
Nico <nico.pfau@icloud.com>
2023-07-16 13:01:59 +0200

    Simple download functionality

--- src/main/java/org/nico/quoted/controller/MarkdownController.java
+++ src/main/java/org/nico/quoted/controller/MarkdownController.java
@@ -2,10 +2,18 @@ package org.nico.quoted.controller;
 
 import org.nico.quoted.service.MarkdownService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -27,6 +35,25 @@ public class MarkdownController {
         }
 
         return "Markdown files generated successfully!";
+    }
+
+    @GetMapping(path = "/generate-markdown/download")
+    public ResponseEntity<Resource> download(String param) throws IOException {
+
+        String filename = "out/The Bible.md";
+
+        Resource resource = new org.springframework.core.io.FileSystemResource(filename);
+
+        // InputStreamResource resource = new InputStreamResource(new FileInputStream(filename));
+
+        // Set the content type and attachment header for the response
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", filename);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(resource);
     }
 }
 
